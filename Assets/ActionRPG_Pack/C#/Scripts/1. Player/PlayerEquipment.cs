bbcde89b178e0d04d479b33235ca83afcdee8d65
zhangyuge <630024962@qq.com>
2020-03-18 23:27:49 +0800

    fix GlobalExpansion.cs

--- Assets/ActionRPG_Pack/C#/Scripts/1. Player/PlayerEquipment.cs
+++ Assets/ActionRPG_Pack/C#/Scripts/1. Player/PlayerEquipment.cs
@@ -4,7 +4,22 @@ using UnityEngine;
 
 public class PlayerEquipment : MonoBehaviour
 {
-    private Dictionary<int, EquipmentVO> euiqpmentDict = new Dictionary<int, EquipmentVO>();
+    private Dictionary<int, EquipmentVO> equipmentDict = new Dictionary<int, EquipmentVO>();
+    private Attribute allEquipmentsAttribute = new Attribute();
+    public Attribute AllEquipmentsAttribute
+    {
+        get
+        {
+            foreach (var item in equipmentDict)
+            {
+                if (item.Value != null)
+                {
+                    allEquipmentsAttribute = GlobalExpansion.GetSumOfAttributes(allEquipmentsAttribute, item.Value);
+                }
+            }
+            return allEquipmentsAttribute;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -16,51 +31,86 @@ public class PlayerEquipment : MonoBehaviour
     {
         
     }
+    /// <summary>
+    /// 换或者穿
+    /// </summary>
+    /// <param name="equipment"></param>
+    /// <param name="pos"></param>
+    public void ChangeEquipment(EquipmentVO equipment, int pos = 0)
+    {
+        if (equipment == null)
+            return;
+        if (pos == 0)
+            pos = equipment.Position;
+
+        equipmentDict[pos] = equipment;
+        EventCenter.Broadcast(EGameEvent.eEquipmentChange);
+    }
+    /// <summary>
+    /// 卸下装备
+    /// </summary>
+    /// <param name="pos"></param>
+    public void PutOffEquipment(int pos)
+    {
+        if (equipmentDict.ContainsKey(pos))
+        {
+            equipmentDict.Remove(pos);
+        }
+        EventCenter.Broadcast( EGameEvent.eEquipmentChange);
+    }
+    public EquipmentVO GetEquipmentByPos(int pos)
+    {
+        if (equipmentDict.ContainsKey(pos))
+        {
+            return equipmentDict[pos];
+        }
+        return null;
+    }
 }
 public class EquipmentVO
 {
     public long UId;
-    public long Position;
+    public int Position;
     public int ConfigId;
 
-    public int Strenght = 1;//力量
-    public int Intelligence = 1;//智力
-    public int Constitution = 1;//体质
-    public int Agility = 1;//敏捷
-    public int Lucky = 1;//幸运
+    public int Strenght;//力量
+    public int Intelligence;//智力
+    public int Constitution;//体质
+    public int Agility;//敏捷
+    public int Lucky;//幸运
 
-    public long Health = 1;//血量
-    public long Mana = 1;//法力值
-    public long Attack = 1;//攻击
-    public long Defense = 1;//防御
-    public long HealthRegen = 1;//回血
-    public long ManaRegen = 1;//回蓝
+    public long Health;//血量
+    public long Mana;//法力值
+    public long Attack;//攻击
+    public long Defense;//防御
+    public long HealthRegen;//回血
+    public long ManaRegen;//回蓝
 
-    public int AtkSpeed = 1;//攻速
-    public int MoveSpeed = 1;//移速
+    public int AtkSpeed;//攻速
+    public int MoveSpeed;//移速
 
-    public int CriticalRate = 1;//暴击率
-    public int CriticalDamageRate = 1;//暴伤率
+    public int CriticalRate;//暴击率
+    public int CriticalDamageRate;//暴伤率
 }
 public class Attribute
 {
-    public int Strenght = 1;//力量
-    public int Intelligence = 1;//智力
-    public int Constitution = 1;//体质
-    public int Agility = 1;//敏捷
-    public int Lucky = 1;//幸运
+    public int Strenght;//力量
+    public int Intelligence;//智力
+    public int Constitution;//体质
+    public int Agility;//敏捷
+    public int Lucky;//幸运
 
-    public long Health = 1;//血量
-    public long Mana = 1;//法力值
-    public long Attack = 1;//攻击
-    public long Defense = 1;//防御
-    public long HealthRegen = 1;//回血
-    public long ManaRegen = 1;//回蓝
+    public long Health;//血量
+    public long Mana;//法力值
+    public long Attack;//攻击
+    public long Defense;//防御
+    public long HealthRegen;//回血
+    public long ManaRegen;//回蓝
 
-    public int AtkSpeed = 1;//攻速
-    public int MoveSpeed = 1;//移速
+    public int AtkSpeed;//攻速
+    public int MoveSpeed;//移速
 
-    public int CriticalRate = 1;//暴击率
-    public int CriticalDamageRate = 1;//暴伤率
+    public int CriticalRate;//暴击率
+    public int CriticalDamageRate;//暴伤率
 }
 
