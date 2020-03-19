5cc7463148d7c425b9e4b8db2c3d34d9fddcee20
6300 <630024962@qq.com>
2020-03-19 09:45:07 +0800

    fix JsonConfig/Enhancement.json

--- Assets/Script/Data/DataManager.cs
+++ Assets/Script/Data/DataManager.cs
@@ -8,8 +8,10 @@ public class DataManager : Singleton<DataManager> {
 
 
     public Dictionary<string, ItemData> itemDataDic = new Dictionary<string, ItemData>();
+    public Dictionary<string, zhiye> zhiyeDataDic = new Dictionary<string, zhiye>();
     public DataManager(){
         Load();
+        LoadZhiye();
     }
     public void Init()
     {
@@ -31,9 +33,36 @@ public class DataManager : Singleton<DataManager> {
             Debug.Log(item.Value.Name);
         }
     }
+    void LoadZhiye()
+    {
+        string filepath = Application.dataPath + "/Resources/JsonConfig/" + "职业1.json";
+        if (!File.Exists(filepath))
+        {
+            return;
+        }
+        StreamReader sr = new StreamReader(filepath, System.Text.Encoding.UTF8);
+        string strLine = sr.ReadToEnd();
+        zhiyeDataDic = JsonMapper.ToObject<Dictionary<string, zhiye>>(strLine);
+        sr.Dispose();
+        foreach (var item in zhiyeDataDic)
+        {
+            Debug.Log(item.Value.hp);
+        }
+    }
 }
 public class ItemData
 {
     public int Id;
     public string Name;
+}
+public class zhiye
+{
+    public int id;
+    public int lv;
+    public double hp;
+    //public List<string> atk;
+    public List<int> fa;
+    public bool jue;
+    public string pu;
+    public List<string> p2u;
 }
---
+++ Assets/Script/GlobalExpansion.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalExpansion
+{
+    public static Attribute GetSumOfAttributes(Attribute a, Attribute b)
+    {
+        a.Strenght += b.Strenght;
+        a.Intelligence += b.Intelligence;
+        a.Constitution += b.Constitution;
+        a.Agility += b.Agility;
+        a.Lucky += b.Lucky;
+
+        a.Health += b.Health;
+        a.Mana += b.Mana;
+        a.Attack += b.Attack;
+        a.Defense += b.Defense;
+        a.HealthRegen += b.HealthRegen;
+        a.ManaRegen += b.ManaRegen;
+
+        a.AtkSpeed += b.AtkSpeed;
+        a.MoveSpeed += b.MoveSpeed;
+
+        a.CriticalRate += b.CriticalRate;
+        a.CriticalDamageRate += b.CriticalDamageRate;
+        return a;
+    }
+    public static Attribute GetSumOfAttributes(Attribute a, EquipmentVO b)
+    {
+        a.Strenght += b.Strenght;
+        a.Intelligence += b.Intelligence;
+        a.Constitution += b.Constitution;
+        a.Agility += b.Agility;
+        a.Lucky += b.Lucky;
+
+        a.Health += b.Health;
+        a.Mana += b.Mana;
+        a.Attack += b.Attack;
+        a.Defense += b.Defense;
+        a.HealthRegen += b.HealthRegen;
+        a.ManaRegen += b.ManaRegen;
+
+        a.AtkSpeed += b.AtkSpeed;
+        a.MoveSpeed += b.MoveSpeed;
+
+        a.CriticalRate += b.CriticalRate;
+        a.CriticalDamageRate += b.CriticalDamageRate;
+        return a;
+    }
+}
